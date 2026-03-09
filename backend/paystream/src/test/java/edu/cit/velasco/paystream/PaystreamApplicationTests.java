9d019c5e4dc6d219490e3f2085bf6681f276072d
LAB24_USER ON WS34 <L24X07W34@LabHCI.net>
2026-03-09 09:40:49 +0800

    Initialize project structure and database connection

---
+++ backend/paystream/src/test/java/edu/cit/velasco/paystream/PaystreamApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.velasco.paystream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
